0da059047876759ebe925acd9801e02dd9bb691b
Celia Fritsch <celia.fritsch@telecomnancy.net>
2022-12-22 09:53:31 +0100

    change template properties and read location in parameters

--- itinerary/src/main/java/it/unitn/fritsch/itinerary/ItineraryController.java
+++ itinerary/src/main/java/it/unitn/fritsch/itinerary/ItineraryController.java
@@ -2,9 +2,7 @@ package it.unitn.fritsch.itinerary;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -12,11 +10,8 @@ import java.util.Map;
 @RequestMapping(path="/api/v1/itinerary")
 public class ItineraryController {
     private final ItineraryServiceGeoapify itineraryServiceGeoapify;
-    private double startLat = 46.06672925347145;
-    private double startLong = 11.149817900662818;
-    private double endLat = 46.06726990853974;
-    private double endLong = 11.121434990336198;
 
+    //http://localhost:9080/api/v1/itinerary/transit?startLat=46.06672925347145&startLong=11.149817900662818&endLat=46.06726990853974&endLong=11.121434990336198
     @Autowired
     public ItineraryController(ItineraryServiceGeoapify itineraryServiceGeoapify) {
         this.itineraryServiceGeoapify = itineraryServiceGeoapify;
@@ -28,25 +23,9 @@ public class ItineraryController {
         //return "Hello world";
     }*/
 
-    @GetMapping("/transit")
-    public Map<String, Integer> getByPublicTransport() throws JsonProcessingException {
-        // Time
-        //Distance
-        // Number of change
-        return itineraryServiceGeoapify.getInfo(startLat, startLong, endLat, endLong, "transit");
-    }
-
-    @GetMapping("/drive")
-    public Map<String, Integer> getByDriving() throws JsonProcessingException {
-        //Time
-        //Distance
-        return itineraryServiceGeoapify.getInfo(startLat, startLong, endLat, endLong, "drive");
-    }
-
-    @GetMapping("/walk")
-    public Map<String, Integer> getByWalk() throws JsonProcessingException {
-        return itineraryServiceGeoapify.getInfo(startLat, startLong, endLat, endLong, "walk");
-
+    @GetMapping("/{mode}")
+    public Map<String, Integer> getRouteInfo(@PathVariable String mode, @RequestParam("startLat") double startLat, @RequestParam("startLong") double startLong, @RequestParam("endLat") double endLat, @RequestParam("endLong") double endLong) throws JsonProcessingException {
+        return itineraryServiceGeoapify.getInfo(startLat, startLong, endLat, endLong, mode);
     }
 
 
